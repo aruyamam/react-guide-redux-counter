c365edf34f94576f803c8ab223e92a51e03a6103
aruyamam <aruyamam@gmail.com>
2018-12-07 14:45:27 +0900

    Connecting React to Redux

--- src/container/Coutner.js
+++ src/container/Coutner.js
@@ -1,35 +1,37 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
 import CounterOutput from '../components/CounterOutput/CounterOutput';
 import CounterControl from '../components/CounterControl/CounterControl';
 
 class Coutner extends Component {
    state = {
-      counter: 0,
+      counter: 0
    };
 
    counterChangeHandler = (action, value) => {
       switch (action) {
          case 'inc':
             this.setState(prevState => ({
-               counter: prevState.counter + 1,
+               counter: prevState.counter + 1
             }));
             break;
 
          case 'dec':
             this.setState(prevState => ({
-               counter: prevState.counter - 1,
+               counter: prevState.counter - 1
             }));
             break;
 
          case 'add':
             this.setState(prevState => ({
-               counter: prevState.counter + value,
+               counter: prevState.counter + value
             }));
             break;
 
          case 'sub':
             this.setState(prevState => ({
-               counter: prevState.counter - value,
+               counter: prevState.counter - value
             }));
             break;
 
@@ -38,12 +40,20 @@ class Coutner extends Component {
    };
 
    render() {
+      const { ctr, onIncrementCounter } = this.props;
+
       return (
          <div>
-            <CounterOutput value={this.state.counter} />
-            <CounterControl label="Increment" clicked={() => this.counterChangeHandler('inc')} />
-            <CounterControl label="Decrement" clicked={() => this.counterChangeHandler('dec')} />
-            <CounterControl label="Add 5" clicked={() => this.counterChangeHandler('add', 5)} />
+            <CounterOutput value={ctr} />
+            <CounterControl label="Increment" clicked={onIncrementCounter} />
+            <CounterControl
+               label="Decrement"
+               clicked={() => this.counterChangeHandler('dec')}
+            />
+            <CounterControl
+               label="Add 5"
+               clicked={() => this.counterChangeHandler('add', 5)}
+            />
             <CounterControl
                label="Subtract 5"
                clicked={() => this.counterChangeHandler('sub', 5)}
@@ -53,4 +63,15 @@ class Coutner extends Component {
    }
 }
 
-export default Coutner;
+const mapStateToProps = state => ({
+   ctr: state.counter
+});
+
+const mapDispatchToProps = dispatch => ({
+   onIncrementCounter: () => dispatch({ type: 'INCREMENT' })
+});
+
+export default connect(
+   mapStateToProps,
+   mapDispatchToProps
+)(Coutner);
