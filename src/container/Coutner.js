f5d50284f7b792efc36d8ae7af3cbf5f3af3bbff
aruyamam <aruyamam@gmail.com>
2018-12-09 12:03:27 +0900

    Updating Immutably

--- src/container/Coutner.js
+++ src/container/Coutner.js
@@ -6,16 +6,20 @@ import CounterControl from '../components/CounterControl/CounterControl';
 
 class Coutner extends Component {
    state = {
-      counter: 0
+      counter: 0,
+      results: []
    };
 
    render() {
       const {
          ctr,
+         storeResults,
          onIncrementCounter,
          onDecrementCounter,
          onAddCounter,
-         onSubtractCounter
+         onSubtractCounter,
+         onStoreResult,
+         onDeleteResult
       } = this.props;
 
       return (
@@ -25,20 +29,37 @@ class Coutner extends Component {
             <CounterControl label="Decrement" clicked={onDecrementCounter} />
             <CounterControl label="Add 5" clicked={onAddCounter} />
             <CounterControl label="Subtract 5" clicked={onSubtractCounter} />
+            <hr />
+            <button onClick={onStoreResult}>Store Result</button>
+            <ul>
+               {storeResults.map(strResult => (
+                  <li
+                     key={strResult.id}
+                     onClick={() => {
+                        onDeleteResult(strResult.id);
+                     }}
+                  >
+                     {strResult.value}
+                  </li>
+               ))}
+            </ul>
          </div>
       );
    }
 }
 
 const mapStateToProps = state => ({
-   ctr: state.counter
+   ctr: state.counter,
+   storeResults: state.results
 });
 
 const mapDispatchToProps = dispatch => ({
    onIncrementCounter: () => dispatch({ type: 'INCREMENT' }),
    onDecrementCounter: () => dispatch({ type: 'DECREMENT' }),
    onAddCounter: () => dispatch({ type: 'ADD', val: 5 }),
-   onSubtractCounter: () => dispatch({ type: 'SUBTRACT', val: 5 })
+   onSubtractCounter: () => dispatch({ type: 'SUBTRACT', val: 5 }),
+   onStoreResult: () => dispatch({ type: 'STORE_RESULT' }),
+   onDeleteResult: id => dispatch({ type: 'DELETE_RESULT', resultElId: id })
 });
 
 export default connect(
